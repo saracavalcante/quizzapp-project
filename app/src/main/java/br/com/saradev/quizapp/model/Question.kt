36bbac04d77ca5a307645d438d2edb79d4a9ca81
saracavalcante <designersaracavalcante@gmail.com>
2022-02-05 10:24:40 -0300

    Add question model

---
+++ app/src/main/java/br/com/saradev/quizapp/model/Question.kt
@@ -0,0 +1,12 @@
+package br.com.saradev.quizapp.model
+
+data class Question(
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnwser: Int
+)
