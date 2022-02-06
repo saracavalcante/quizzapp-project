16e9dcc81a4b4c65f6593ba120341e652b0597ef
saracavalcante <designersaracavalcante@gmail.com>
2022-02-06 16:26:23 -0300

    Conecte the UI to the model and update answers options

--- app/src/main/java/br/com/saradev/quizapp/fragments/QuizFragment.kt
+++ app/src/main/java/br/com/saradev/quizapp/fragments/QuizFragment.kt
@@ -6,19 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.saradev.quizapp.R
+import br.com.saradev.quizapp.databinding.FragmentQuizBinding
 import br.com.saradev.quizapp.model.Question
 
 class QuizFragment : Fragment() {
 
+    private var _binding: FragmentQuizBinding? = null
+    private val binding: FragmentQuizBinding get() = _binding!!
+
+    private val currentPosition = 1
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
-        flags()
+    ): View = FragmentQuizBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setQuestion()
+        configureProgressBar()
     }
 
-    private fun flags() {
+    private fun setQuestion() {
         val questionList = Question.getQuestions()
+        val question: Question? = questionList[currentPosition - 1]
+
+        binding.apply {
+            tvQuestion.text = question!!.question
+            ivFlag.setImageResource(question.image)
+
+            optionOne.text = question.optionOne
+            optionTwo.text = question.optionTwo
+            optionThree.text = question.optionThree
+            optionFour.text = question.optionFour
+            optionFive.text = question.optionFive
+        }
+    }
+
+    private fun configureProgressBar() {
+        binding.apply {
+            progressBar.progress = currentPosition
+            tvProgress.text = "$currentPosition/${progressBar.max}"
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
--- app/src/main/java/br/com/saradev/quizapp/model/Question.kt
+++ app/src/main/java/br/com/saradev/quizapp/model/Question.kt
@@ -9,6 +9,8 @@ data class Question(
     val optionOne: String,
     val optionTwo: String,
     val optionThree: String,
+    val optionFour: String,
+    val optionFive: String,
     val correctAnswer: Int
 ) {
 
@@ -17,93 +19,113 @@ data class Question(
         fun getQuestions() = listOf(
             Question(
                 id = 1,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_argentina,
                 optionOne = "Armenia",
                 optionTwo = "Australia",
                 optionThree = "Argentina",
+                optionFour = "Honduras",
+                optionFive = "El Salvador",
                 correctAnswer = 3
             ),
             Question(
                 id = 2,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_australia,
                 optionOne = "New Zeland",
                 optionTwo = "Australia",
                 optionThree = "England",
+                optionFour = "United States",
+                optionFive = "Iceland",
                 correctAnswer = 2
             ),
             Question(
                 id = 3,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_brazil,
                 optionOne = "Brazil",
                 optionTwo = "Belarus",
                 optionThree = "Brunei",
+                optionFour = "Bolivia",
+                optionFive = "Barbados",
                 correctAnswer = 1
             ),
             Question(
                 id = 4,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_belgium,
                 optionOne = "Barbados",
                 optionTwo = "Bahamas",
                 optionThree = "Belgium",
+                optionFour = "Belize",
+                optionFive = "Bangladesh",
                 correctAnswer = 3
             ),
             Question(
                 id = 5,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_fiji,
                 optionOne = "Fiji",
                 optionTwo = "Finland",
                 optionThree = "Gabon",
+                optionFour = "France",
+                optionFive = "England",
                 correctAnswer = 1
             ),
             Question(
                 id = 6,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_denmark,
                 optionOne = "Egypt",
                 optionTwo = "Ethiopia",
                 optionThree = "Denmark",
+                optionFour = "Dominica",
+                optionFive = "Djibouti",
                 correctAnswer = 3
             ),
             Question(
                 id = 7,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_india,
                 optionOne = "Hungary",
                 optionTwo = "India",
                 optionThree = "Iran",
+                optionFour = "Indonesia",
+                optionFive = "Ireland",
                 correctAnswer = 2
             ),
             Question(
                 id = 8,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_new_zealand,
                 optionOne = "Australia",
                 optionTwo = "Tuvalu",
                 optionThree = "New Zealand",
+                optionFour = "Netherlands",
+                optionFive = "Nicaragua",
                 correctAnswer = 3
             ),
             Question(
                 id = 9,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_kuwait,
                 optionOne = "Kuwait",
                 optionTwo = "Jordan",
                 optionThree = "Sudan",
+                optionFour = "Kiribati",
+                optionFive = "Kenya",
                 correctAnswer = 1
             ),
             Question(
                 id = 10,
-                question = "What country this flag belong to?",
+                question = "What country does this flag belong to?",
                 image = R.drawable.ic_flag_of_germany,
                 optionOne = "Georgia",
                 optionTwo = "Belgium",
                 optionThree = "Germany",
-                correctAnswer = 1
+                optionFour = "Guinea",
+                optionFive = "Guyana",
+                correctAnswer = 3
             )
         )
     }
